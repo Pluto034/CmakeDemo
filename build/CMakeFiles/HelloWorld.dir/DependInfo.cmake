b492229a115b23398c3c0414d640d97d028f1754
root <root@localhost.localdomain>
2024-09-23 05:08:55 -0700

    Cmake init

---
+++ build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pluto/demo/hello.cpp" "CMakeFiles/HelloWorld.dir/hello.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/hello.cpp.o.d"
+  "/home/pluto/demo/main.cpp" "CMakeFiles/HelloWorld.dir/main.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/main.cpp.o.d"
+  "/home/pluto/demo/world.cpp" "CMakeFiles/HelloWorld.dir/world.cpp.o" "gcc" "CMakeFiles/HelloWorld.dir/world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HelloWorld.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/HelloWorld.dir/main.cpp.o: /home/pluto/demo/main.cpp \
+ /usr/include/stdc-predef.h /home/pluto/demo/hello.h \
+ /home/pluto/demo/world.h
